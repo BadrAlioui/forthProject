b8c7ba169f73c31e4d59163f9bb2a58057043966
Badr Aliloui <tranquil2021@hotmail.com>
2024-08-30 08:44:38 +0100

    add message in setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,6 +125,14 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'studentinstitute2024@gmail.com'
+EMAIL_HOST_PASSWORD = 'fmve ttzx tcjx luec'  # Utilise un mot de passe d'application
+DEFAULT_FROM_EMAIL = 'studentinstitute2024@gmail.com'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
