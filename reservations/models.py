cc382b0fa267b7d725b9c6acfc87518c74a43b25
Badr Aliloui <tranquil2021@hotmail.com>
2024-09-27 11:00:38 +0100

    allowed only the same user to edit or delete once and manke more obvious the reservation button

--- reservations/models.py
+++ reservations/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
--- reservations/views.py
+++ reservations/views.py
@@ -12,13 +12,15 @@ def reservation_page(request):
     reservations = Reservation.objects.all()
     return render(request, "reservations/reservations.html", context={'reservations':reservations})
 
-
+@login_required(login_url="/accounts/login/")
 def liste_reservation(request):
     
     if request.method == "POST":
         form = ReservationForm(request.POST)
         if form.is_valid():
-            form.save()
+            reservation = form.save(commit=False)
+            reservation.user = request.user
+            reservation.save()
             messages.success(request, "Booking has been added")
             return redirect('home')  # Redirect back to the home page
         else:
@@ -27,6 +29,8 @@ def liste_reservation(request):
         form = ReservationForm()  # If it's a GET request, instantiate a blank form
     
     reservations = Reservation.objects.all()  # Fetch all reservations
+    # reservations = Reservation.objects.filter(user=request.user) ... to filter reservations by user
+
 
     return render(request, "reservations/liste_reservation.html", {
         "form": form,
@@ -35,24 +39,32 @@ def liste_reservation(request):
 
 @login_required(login_url="/accounts/login/")
 def edit(request, list_id):
-    # Obtain the reservation to be edited
-    get_reservation = get_object_or_404(Reservation, pk=list_id)
-    
+    try:
+        get_reservation = Reservation.objects.get(pk=list_id)
+    except Reservation.DoesNotExist:
+        messages.error(request, "Reservation not found.")
+        return redirect('reservations:liste')
+
+    if get_reservation.user != request.user:
+        messages.error(request, "You are not allowed to edit this reservation.")
+        return redirect('reservations:liste')
+
     if request.method == "POST":
         form = ReservationForm(request.POST, instance=get_reservation)
         if form.is_valid():
             form.save()
             messages.success(request, "Booking has been edited")
-            return redirect('reservations:liste')  # lead to the list of reservations after editing
+            return redirect('reservations:liste')
         else:
-            messages.error(request, "There was an error in your submission or the reservation already exists.")
+            messages.error(request, "There was an error in your submission.")
     else:
         form = ReservationForm(instance=get_reservation)
-    
+
     return render(request, "reservations/edit_reservation.html", {
         "get_reservation": get_reservation,
         "form": form
     })
+
 
 @login_required(login_url="/accounts/login/")
 def delete_reservation(request):
@@ -61,7 +73,7 @@ def delete_reservation(request):
         last_name = request.POST.get('last_name')
         
         # research reservation
-        reservation = get_object_or_404(Reservation, first_name=first_name, last_name=last_name)
+        reservation = get_object_or_404(Reservation, first_name=first_name, last_name=last_name, user=request.user)
         
         # delelte reservation
         reservation.delete()
