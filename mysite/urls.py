138ab70384998babf2b2b4fdc2b487d71ff6685b
Badr Aliloui <tranquil2021@hotmail.com>
2024-10-06 08:20:38 +0100

    improve the script and add pictures

--- mysite/urls.py
+++ mysite/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     path('menus/', include('menus.urls')),
     path("accounts/", include("allauth.urls")),
     path('reservations/', include('reservations.urls')),
-] 
+]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
