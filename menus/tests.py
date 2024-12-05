c35a20dd93e5d2a10fa4ea573fac4d4ff5c59dfd
Badr Aliloui <tranquil2021@hotmail.com>
2024-12-05 09:11:20 +0000

    Implemented basic test for menu models

--- menus/tests.py
+++ menus/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from menus.models import Menu
 
-# Create your tests here.
+
+class MenuTestCase(TestCase):
+    def test_create_menu(self):
+        Menu.objects.create(
+            title="Couscous",
+            content="Doe",
+            slug="sluge",
+            price=4,
+            
+        )
+        self.assertEqual(Menu.objects.count(), 1)
+        self.assertEqual(Menu.objects.get().title, "Couscous")
+        self.assertEqual(Menu.objects.get().content, "Doe")
+        self.assertEqual(Menu.objects.get().price, 4)
+
