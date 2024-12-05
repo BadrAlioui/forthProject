ed9ead78f5cffa0c89cdae7479787332c6286020
Badr Aliloui <tranquil2021@hotmail.com>
2024-12-05 16:05:50 +0000

    Added reference for the test

--- menus/tests.py
+++ menus/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from menus.models import Menu
 
-
+#https://www.google.com/search?sca_esv=3ddc15aac6af2d0d&rlz=1C1GCCA_en&udm=7&sxsrf=ADLYWIJqjj1gWh2bxlgsTAhGw5YL7JLlRw:1733410165030&q=test.py+models.py&sa=X&ved=2ahUKEwjss4uJ8JCKAxVz_rsIHcQ3A6sQ8ccDegQIERAF&biw=1280&bih=585&dpr=1.5#fpstate=ive&vld=cid:af87c5c4,vid:GBgRMdjAx_c,st:0
 class MenuTestCase(TestCase):
     def test_create_menu(self):
         Menu.objects.create(
