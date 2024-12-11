11b3ff786f9839070159489445c1ff82d26c9564
Badr Aliloui <tranquil2021@hotmail.com>
2024-12-11 19:32:38 +0000

    Fix: Update reservation model to handle unique validation during edit

--- reservations/models.py
+++ reservations/models.py
@@ -3,7 +3,6 @@ from django.utils.timezone import now
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
-
 class Reservation(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, null=True, blank=True
@@ -11,7 +10,7 @@ class Reservation(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
-    number_of_persons = models.IntegerField()  # Changement ici
+    number_of_persons = models.IntegerField()
     date_booking = models.DateTimeField()
     time = models.TimeField(default='00:00')
 
@@ -26,25 +25,26 @@ class Reservation(models.Model):
         self.first_name = self.first_name.lower()
         self.last_name = self.last_name.lower()
 
-        # Vérifier que number_of_persons est défini et valide
-        if self.number_of_persons is None:
-            raise ValidationError("The number of persons is required.")
-        if self.number_of_persons <= 0:
-            raise ValidationError("The number of persons must be greater than 0.")
-
-        # Calculer le total des réservations
-        total_persons = Reservation.objects.filter(
-            date_booking=self.date_booking
-        ).aggregate(total=models.Sum('number_of_persons'))['total'] or 0
-
-        # Vérifier la capacité maximale
-        if total_persons + self.number_of_persons > 15:
-            raise ValidationError("The restaurant is full for this date!")
-
-        # Vérifier les doublons
+        # Vérifier les doublons en excluant l'instance actuelle
         if Reservation.objects.filter(
             first_name=self.first_name,
             last_name=self.last_name,
             date_booking=self.date_booking
-        ).exists():
+        ).exclude(pk=self.pk).exists():
             raise ValidationError("You have already booked a table for this date!")
+
+        # Vérifier que number_of_persons est défini et valide
+        if self.number_of_persons is None:
+            raise ValidationError("The number of persons is required.")
+
+        if self.number_of_persons <= 0:
+            raise ValidationError("The number of persons must be greater than 0.")
+
+        # Calculer le total des réservations en excluant l'instance actuelle
+        total_persons = Reservation.objects.filter(
+            date_booking=self.date_booking
+        ).exclude(pk=self.pk).aggregate(total=models.Sum('number_of_persons'))['total'] or 0
+
+        # Vérifier la capacité maximale
+        if total_persons + self.number_of_persons > 15:
+            raise ValidationError("The restaurant is full for this date!")
