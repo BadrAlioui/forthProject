ae25c5d00a4b8a833c37547a366887f9771d07f5
Badr Aliloui <tranquil2021@hotmail.com>
2025-03-15 15:29:01 +0000

    Removed a space useless

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,8 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount',
-    
+    'allauth.socialaccount',    
     'menus',
     'reservations',
     'cloudinary'
